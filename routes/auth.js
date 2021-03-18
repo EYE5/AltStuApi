b479ced15d61bdaec2e6e2b29f5d50dfb720fb3b
Daniil Antsupov <antsupov.official@gmail.com>
2021-03-19 02:13:13 +0700

    Small changes of session check

--- routes/auth.js
+++ routes/auth.js
@@ -16,6 +16,10 @@ async function auth(login, password, res) {
 
     return curl.request({ url: 'https://student.altstu.ru/login/', method: 'POST', data: {login, password}, include: true }, (err, parts) => {     
         const idx = parts.indexOf('sessionid=');
+
+        const session = parts.slice(idx + 10, idx + 42);
+        if (session[0] === '0')
+            return res.status(418).send({error:'Данные для авторизации не верны'})
         
         return res.send({ session: parts.slice(idx + 10, idx + 42) })
     })
